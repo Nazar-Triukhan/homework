42c60837bd9a1d826db2c891003509877a78b3a1
Nazar <tryuhann@gmail.com>
2025-09-02 09:00:38 +0300

    add

---
+++ index.js
@@ -0,0 +1,18 @@
+const age = 16;
+console.log(age);
+
+const name = 'Nazik';
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = 'Немає улюбленої цитати';
+console.log(myString);
+
+let myNumber = 10;
+myNumber = myNumber +10;
+console.log(myNumber);
+
+const  myNul = null;
+console.log( myNul);
