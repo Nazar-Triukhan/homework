efca207231d0dfae14ddc29fd9b446535ef88cec
Nazar <tryuhann@gmail.com>
2025-11-28 15:16:05 +0200

    add

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// Використати деструктуризацію об’єктів у всіх завданнях з попередньої ДЗ
 const user = {
     hobby: 'football',
     premium: true,
 }
+const {hobby: interesting,premium: many} = user
 user.mood = 'happy'
 user.hobby = 'skydiving'
 user.premium = false
 const k = Object.keys(user)
 for(let i of k){
-    console.log(i,user[i]);
-    
+    console.log(i,user[i]); 
 }
 
 // Завдання 2
@@ -17,10 +18,10 @@ const userInf = {
     premium: true,
     mood: 'happy'
 }
+const {hobby,premium,mood} = userInf
 const countProps = (obj) => {
 const key = Object.keys(obj)
 return key.length
-
 }
 console.log(countProps(userInf));
 // Завдання 3
@@ -32,6 +33,7 @@ const obj = {
     maks: 1,
 
 }
+const {vlad,nazar,sahon,artem,maks} = obj
 const findBestEmployee = (employees) => {
 let bestname = ''
 let maxtask = 0
@@ -54,6 +56,7 @@ const userMany = {
     artem: 50,
     maks: 500,
 }
+const {vlad:vladUser,nazar:nazarUser,sahon:sahonUser,artem:artemUser,maks:maksUser} = userMany
 const  countTotalSalary = (employees) => {
     
 let sum = 0
@@ -69,6 +72,7 @@ console.log(countTotalSalary(userMany));
 
 // Завдання 5
 const nameUser = [{name: 'Nazar'},{name:'Vlad'},{name:'Sahon'}]
+const [elem1,elem2,elem3] = nameUser
 const key = Object.keys(nameUser[0])[0];
 
 const getAllPropValues = (arr, prop) => {
@@ -89,7 +93,11 @@ const infoMany = [
     {name: 'Tort',many:200,skilki: 2},
     {name: 'Scitls',many:10,skilki: 10},
 ]
-
+const [
+    {name:name1,many:many1,skilki:skilki1},
+    {name:name2,many:many2,skilki:skilki2},
+    {name:name3,many:many3,skilki:skilki3}
+] = infoMany
 const calculateTotalPrice = (allProdcuts, productName) => {
     let sum = 0
 for(let i = 0;i < allProdcuts.length;i++){
@@ -103,3 +111,123 @@ console.log(calculateTotalPrice(infoMany, 'Aple'));
 console.log(calculateTotalPrice(infoMany, 'Tort'));
 console.log(calculateTotalPrice(infoMany, 'Scitls'));
 
+
+
+
+
+
+
+
+
+
+
+/*
+ * Типів транзацкій всього два.
+ * Можна покласти або зняти гроші з рахунку.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Кожна транзакція - це об'єкт з властивостями: id, type і amount
+ */
+const account = {
+  // Поточний баланс рахунку
+  balance: 0,
+
+  // Історія транзакцій
+
+  transactions: [],
+  id:1,
+  /*
+   * Метод створює і повертає об'єкт транзакції.
+   * Приймає суму і тип транзакції.
+   */
+
+  createTransaction(amount, type) {
+    const transaction = {
+            amount:amount,
+            type:type,
+            id:this.id
+    }
+    this.id += 1
+    return transaction
+  },
+  /*
+   * Метод відповідає за додавання суми до балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій
+   */
+  deposit(amount) {
+    this.balance += amount
+    const transaction =  this.createTransaction(amount,Transaction.DEPOSIT)
+    this.transactions.push(transaction)
+  },
+
+  /*
+   * Метод відповідає за зняття суми з балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій.
+   *
+   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
+   * про те, що зняття такої суми не можливо, недостатньо коштів.
+   */
+  withdraw(amount) {
+    if(amount > this.balance){
+        console.log('зняття такої суми не можливо,недостатньо коштів');
+        return
+    }
+    this.balance -= amount
+    const transaction = this.createTransaction(amount,Transaction.WITHDRAW)
+    this.transactions.push(transaction)
+  },
+
+  /*
+   * Метод повертає поточний баланс
+   */
+  getBalance() {
+    return this.balance
+  },
+  /*
+   * Метод шукає і повертає об'єкт транзакції по id
+   */
+  getTransactionDetails(id) {
+    for(let i = 0;i < this.transactions.length;i++){
+        if(this.transactions[i].id === id ){
+            return this.transactions[i]
+        }
+    }
+    return null
+  },
+
+  /*
+
+   * Метод повертає кількість коштів
+   * певного типу транзакції з усієї історії транзакцій
+   */
+  getTransactionTotal(type) {
+    let total = 0 
+    for(let i = 0;i < this.transactions.length;i++){
+        if(this.transactions[i].type === type ){
+            total += this.transactions[i].amount
+        }
+    }
+    return total
+  },
+};
+
+
+
+account.deposit(1000)
+account.deposit(5000)
+account.withdraw(1000)
+account.withdraw(1000)
+account.withdraw(2000)
+console.log(account.getBalance());
+console.log(account.getTransactionDetails(4));
+console.log(account.getTransactionTotal('withdraw'));
+console.log(account);
