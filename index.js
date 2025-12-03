d62f427fcb754791a94d05007a0c2944de45a7d7
Nazar <tryuhann@gmail.com>
2025-12-03 16:23:48 +0200

    add

--- index.js
+++ index.js
@@ -1,233 +1,207 @@
-// Використати деструктуризацію об’єктів у всіх завданнях з попередньої ДЗ
-const user = {
-    hobby: 'football',
-    premium: true,
-}
-const {hobby: interesting,premium: many} = user
-user.mood = 'happy'
-user.hobby = 'skydiving'
-user.premium = false
-const k = Object.keys(user)
-for(let i of k){
-    console.log(i,user[i]); 
-}
+const users = [
+
+  {
+
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+
+    name: 'Moore Hensley',
+
+    email: 'moorehensley@indexia.com',
+
+    eyeColor: 'blue',
+
+    friends: ['Sharron Pace'],
+
+    isActive: false,
+
+    balance: 2811,
+
+    skills: ['ipsum', 'lorem'],
+
+    gender: 'male',
+
+    age: 37,
+
+  },
+
+  {
+
+    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+
+    name: 'Sharlene Bush',
+
+    email: 'sharlenebush@tubesys.com',
+
+    eyeColor: 'blue',
+
+    friends: ['Briana Decker', 'Sharron Pace'],
+
+    isActive: true,
+
+    balance: 3821,
+
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+
+    gender: 'female',
+
+    age: 34,
+
+  },
+
+  {
+
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+
+    name: 'Ross Vazquez',
+
+    email: 'rossvazquez@xinware.com',
+
+    eyeColor: 'green',
+
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+
+    isActive: false,
+
+    balance: 3793,
+
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+
+    gender: 'male',
+
+    age: 24,
+
+  },
+
+  {
+
+    id: '249b6175-5c30-44c6-b154-f120923736f5',
+
+    name: 'Elma Head',
+
+    email: 'elmahead@omatom.com',
+
+    eyeColor: 'green',
+
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+
+    isActive: true,
+
+    balance: 2278,
+
+    skills: ['adipisicing', 'irure', 'velit'],
+
+    gender: 'female',
+
+    age: 21,
+
+  },
+
+  {
+
+    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+
+    name: 'Carey Barr',
+
+    email: 'careybarr@nurali.com',
+
+    eyeColor: 'blue',
+
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+
+    isActive: true,
+
+    balance: 3951,
+
+    skills: ['ex', 'culpa', 'nostrud'],
+
+    gender: 'male',
+
+    age: 27,
+
+  },
+
+  {
+
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+
+    name: 'Blackburn Dotson',
+
+    email: 'blackburndotson@furnigeer.com',
+
+    eyeColor: 'brown',
+
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+
+    isActive: false,
+
+    balance: 1498,
+
+    skills: ['non', 'amet', 'ipsum'],
+
+    gender: 'male',
+
+    age: 38,
+
+  },
+
+  {
+
+    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+
+    name: 'Sheree Anthony',
+
+    email: 'shereeanthony@kog.com',
+
+    eyeColor: 'brown',
+
+    friends: ['Goldie Gentry', 'Briana Decker'],
+
+    isActive: true,
+
+    balance: 2764,
+
+    skills: ['lorem', 'veniam', 'culpa'],
+
+    gender: 'female',
+
+    age: 39,
+
+  },
+
+];
+
+const getUserNames = users => users.map((elem) => elem.name);
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
 
 // Завдання 2
-const userInf = {
-    hobby: 'football',
-    premium: true,
-    mood: 'happy'
-}
-const {hobby,premium,mood} = userInf
-const countProps = (obj) => {
-const key = Object.keys(obj)
-return key.length
-}
-console.log(countProps(userInf));
-// Завдання 3
-const obj = {
-    vlad: 3,
-    nazar: 111,
-    sahon: 3,
-    artem: 37,
-    maks: 1,
+// Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
+const getUsersWithEyeColor = (users, color) => users.filter(elem => elem.eyeColor === color);
+console.log(getUsersWithEyeColor(users, 'blue')); // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
 
-}
-const {vlad,nazar,sahon,artem,maks} = obj
-const findBestEmployee = (employees) => {
-let bestname = ''
-let maxtask = 0
-const entry = Object.entries(employees)
-for (const i of entry) {
-    if(i[1] > maxtask){
-        maxtask = i[1]
-        bestname = i[0]
-    }
-}
-return `best name: ${bestname} task: ${maxtask}`
-}
-console.log(findBestEmployee(obj));
+
+
+// Завдання 3
+// Отримати масив імен користувачів за статтю (поле gender)
+const getUsersWithGender = (users, gender) => users.filter(elem => elem.gender === gender).map(item => item.name);
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
 
 // Завдання 4
-const userMany = {
-    vlad: 100,
-    nazar: 150,
-    sahon: 200,
-    artem: 50,
-    maks: 500,
-}
-const {vlad:vladUser,nazar:nazarUser,sahon:sahonUser,artem:artemUser,maks:maksUser} = userMany
-const  countTotalSalary = (employees) => {
-    
-let sum = 0
-for(const i in employees) {
-    sum += employees[i]
-}
-return sum
+// Отримати масив тільки неактивних користувачів (поле isActive).
+const getInactiveUsers = users => {
+    return users.filter(item => !item.isActive)
 
-}
-
-console.log(countTotalSalary(userMany));
+};
+console.log(getInactiveUsers(users)); // [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
 
 
 // Завдання 5
-const nameUser = [{name: 'Nazar'},{name:'Vlad'},{name:'Sahon'}]
-const [elem1,elem2,elem3] = nameUser
-const key = Object.keys(nameUser[0])[0];
-
-const getAllPropValues = (arr, prop) => {
-
-    let userName = []
-for(let i = 0;i < arr.length;i++){
-    userName.push(arr[i][prop])
-}
-return userName
-
-}
-console.log(getAllPropValues(user,key));
-
-// Завдання 6
-
-const infoMany = [
-    {name: 'Aple',many:20,skilki: 5},
-    {name: 'Tort',many:200,skilki: 2},
-    {name: 'Scitls',many:10,skilki: 10},
-]
-const [
-    {name:name1,many:many1,skilki:skilki1},
-    {name:name2,many:many2,skilki:skilki2},
-    {name:name3,many:many3,skilki:skilki3}
-] = infoMany
-const calculateTotalPrice = (allProdcuts, productName) => {
-    let sum = 0
-for(let i = 0;i < allProdcuts.length;i++){
-if (allProdcuts[i].name === productName) {
-    sum += allProdcuts[i].many * allProdcuts[i].skilki
-}
-}
-return sum
-}
-console.log(calculateTotalPrice(infoMany, 'Aple'));
-console.log(calculateTotalPrice(infoMany, 'Tort'));
-console.log(calculateTotalPrice(infoMany, 'Scitls'));
-
-
-
-
-
-
-
-
-
-
-
-/*
- * Типів транзацкій всього два.
- * Можна покласти або зняти гроші з рахунку.
- */
-const Transaction = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
+// Отримати користувача (не масив) по email (поле email, він унікальний).
+// Отримати користувача (не масив) по email (поле email, він унікальний).
+const getUserWithEmail = (users, email) => {
+  return users.find(item => item.email === email)
 };
-
-/*
- * Кожна транзакція - це об'єкт з властивостями: id, type і amount
- */
-const account = {
-  // Поточний баланс рахунку
-  balance: 0,
-
-  // Історія транзакцій
-
-  transactions: [],
-  id:1,
-  /*
-   * Метод створює і повертає об'єкт транзакції.
-   * Приймає суму і тип транзакції.
-   */
-
-  createTransaction(amount, type) {
-    const transaction = {
-            amount:amount,
-            type:type,
-            id:this.id
-    }
-    this.id += 1
-    return transaction
-  },
-  /*
-   * Метод відповідає за додавання суми до балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій
-   */
-  deposit(amount) {
-    this.balance += amount
-    const transaction =  this.createTransaction(amount,Transaction.DEPOSIT)
-    this.transactions.push(transaction)
-  },
-
-  /*
-   * Метод відповідає за зняття суми з балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій.
-   *
-   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
-   * про те, що зняття такої суми не можливо, недостатньо коштів.
-   */
-  withdraw(amount) {
-    if(amount > this.balance){
-        console.log('зняття такої суми не можливо,недостатньо коштів');
-        return
-    }
-    this.balance -= amount
-    const transaction = this.createTransaction(amount,Transaction.WITHDRAW)
-    this.transactions.push(transaction)
-  },
-
-  /*
-   * Метод повертає поточний баланс
-   */
-  getBalance() {
-    return this.balance
-  },
-  /*
-   * Метод шукає і повертає об'єкт транзакції по id
-   */
-  getTransactionDetails(id) {
-    for(let i = 0;i < this.transactions.length;i++){
-        if(this.transactions[i].id === id ){
-            return this.transactions[i]
-        }
-    }
-    return null
-  },
-
-  /*
-
-   * Метод повертає кількість коштів
-   * певного типу транзакції з усієї історії транзакцій
-   */
-  getTransactionTotal(type) {
-    let total = 0 
-    for(let i = 0;i < this.transactions.length;i++){
-        if(this.transactions[i].type === type ){
-            total += this.transactions[i].amount
-        }
-    }
-    return total
-  },
-};
-
-
-
-account.deposit(1000)
-account.deposit(5000)
-account.withdraw(1000)
-account.withdraw(1000)
-account.withdraw(2000)
-console.log(account.getBalance());
-console.log(account.getTransactionDetails(4));
-console.log(account.getTransactionTotal('withdraw'));
-console.log(account);
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {об'єкт користувача Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {об'єкт користувача Elma Head}
